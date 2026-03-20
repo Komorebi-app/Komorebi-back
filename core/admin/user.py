d0541747bb7ef9248ed97e3dec11aef4c920514f
Namania <mael.namania@gmail.com>
2026-03-20 12:06:50 +0100

    fix: style

--- core/admin/user.py
+++ core/admin/user.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from unfold.admin import ModelAdmin
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
 class UserAdmin(BaseUserAdmin, ModelAdmin):
-    pass
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
--- komorebi/settings.py
+++ komorebi/settings.py
@@ -86,6 +86,21 @@ UNFOLD = {
         'light': '/static/images/logo.webp',
         'dark': '/static/images/logo.webp',
     },
+    'COLORS': {
+        'primary': {
+            '50': '#ecfdf5',
+            '100': '#d1fae5',
+            '200': '#a7f3d0',
+            '300': '#6ee7b7',
+            '400': '#34d399',
+            '500': '#10b981',
+            '600': '#059669',
+            '700': '#047857',
+            '800': '#065f46',
+            '900': '#064e3b',
+            '950': '#022c22',
+        },
+    },
     'SITE_FAVICONS': [
         {
             'rel': 'icon',
@@ -94,6 +109,7 @@ UNFOLD = {
             'href': '/static/images/logo.webp',
         },
     ],
+    'BORDER_RADIUS': '10px',
     'SIDEBAR': {
         'show_search': True,
         'navigation': [
