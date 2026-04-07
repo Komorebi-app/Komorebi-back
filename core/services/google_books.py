a7e7033f3704cb8fae8fdb5a5c264b35de08a96c
Théo danel <theo.danel@ecoles-epsi.net>
2026-04-07 22:52:19 +0200

    feat: add search functionality for books by title using Google Books API

--- core/services/google_books.py
+++ core/services/google_books.py
@@ -52,3 +52,58 @@ def fetch_google_book_by_isbn(isbn: str):
 
     # Livre trouvé
     return volume_info
+
+
+
+def fetch_google_book_by_title(title: str):
+    # Vérifie la configuration locale
+    api_key = getattr(settings, "GOOGLE_BOOKS_API_KEY", "")
+    if not api_key:
+        raise GoogleBooksNotConfiguredError("GOOGLE_BOOKS_API_KEY manquant")
+    
+    # Appelle Google Books
+    try:
+        response = requests.get(
+            GOOGLE_BOOKS_BASE_URL,
+            params={"q": f"intitle:{title}", "key": api_key},
+            timeout=GOOGLE_BOOKS_REQUEST_TIMEOUT,
+        )
+        response.raise_for_status()
+    except requests.RequestException as exc:
+        raise GoogleBooksRequestError("Erreur lors de l'appel Google Books") from exc
+    
+    # Analyse la réponse
+    try:
+        data = response.json()
+    except ValueError as exc:
+        raise GoogleBooksRequestError("Réponse JSON invalide de Google Books") from exc
+
+    try:
+        # Si Google ne trouve pas de livre
+        if not data.get("items"):
+            return []
+
+        # On extrait le volumeInfo de chaque livre trouvé
+        books = []
+        for item in data["items"]:
+            volume_info = item.get("volumeInfo")
+            identifiers = volume_info.get('industryIdentifiers', [])
+            
+            # On cherche l'ISBN_13 d'abord, sinon l'ISBN_10
+            isbn_val = next((id['identifier'] for id in identifiers if id['type'] in ['ISBN_13', 'ISBN_10']), None)
+                
+            # On n'ajoute le livre à la liste que s'il a un ISBN pour pouvoir dans le futur l'ajouter à la BDD via le front
+            if isbn_val:
+                volume_info['isbn'] = isbn_val # On ajoute l'ISBN au volume_info pour faciliter son utilisation côté front ( pour un futur ajout de la part de l'utilisateur)
+                books.append(volume_info)
+                
+            # si pas d'isbn, books.append n'est pas appelé et le livre n'est pas ajouté à la liste des résultats
+
+        if not books:
+            raise GoogleBooksRequestError("Le champ 'volumeInfo' est manquant")
+    except (AttributeError, KeyError, IndexError, TypeError) as exc:
+        raise GoogleBooksRequestError("Format de réponse Google Books invalide") from exc
+
+    # Livres trouvés
+    return books
+
