db1c88f04e41f13de9315976f641e487a360f9a2
Namania <mael.namania@gmail.com>
2026-04-09 01:54:51 +0200

    feat: user registration

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,4 +2,4 @@ from .author import AuthorSerializer
 from .book import BookSerializer
 from .library import LibrarySerializer
 from .rating import RatingSerializer
-from .user import UserSerializer
+from .user import UserSerializer, UserRegisterSerializer
--- core/views/user.py
+++ core/views/user.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import User
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
-from core.serializers import UserSerializer
+from core.serializers import UserSerializer, UserRegisterSerializer
 from core.utils.query import get_query_all_for_user
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -13,6 +13,8 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
     def get_permissions(self):
+        if self.action in ["new"]:
+            return [AllowAny()]
         if self.action in ["me"]:
             return [IsAuthenticated()]
         return [IsAdminUser()]
@@ -24,3 +26,11 @@ class UserViewSet(viewsets.ModelViewSet):
     def me(self, request):
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
+
+    @action(detail=False, methods=['post'], serializer_class=UserRegisterSerializer)
+    def new(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
