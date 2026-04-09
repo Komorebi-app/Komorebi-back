44bfd30a969579d9db085d978c542b2fb2fd3d21
Namania <mael.namania@gmail.com>
2026-04-10 00:07:21 +0200

    fix: author serializer in book resource

--- core/serializers/book.py
+++ core/serializers/book.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 
 from core.models import Book
+from .author import AuthorSerializer
 
 class BookSerializer(serializers.HyperlinkedModelSerializer):
+    author = AuthorSerializer(read_only=True)
+
     class Meta:
         model = Book
         fields = ['id', 'title', 'cover_url', 'author', 'isbn', 'language', 'pages', 'published', 'resume']
