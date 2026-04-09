42227a23b3ce4064412e918582cc5f5bbbf55a02
Namania <mael.namania@gmail.com>
2026-04-09 16:13:43 +0200

    fix: remove media viewset import

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,6 +1,5 @@
 from .author import AuthorViewSet
 from .book import BookViewSet
 from .library import LibraryViewSet
-from .media import MediaViewSet
 from .rating import RatingViewSet
 from .user import UserViewSet
